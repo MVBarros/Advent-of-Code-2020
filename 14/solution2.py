efe443cff5dc4883efbd9d339632ea7a0682badd
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2020-12-21 23:35:27 +0000

    Add solution for second part of problem 14

--- 14/solution2.py
+++ 14/solution2.py
@@ -4,16 +4,23 @@ import itertools
 def bin_or(x, y):
     return '{0:036b}'.format(int(x, 2) | int(y, 2))
 
-def bin_and(x, y):
-    return '{0:036b}'.format(int(x, 2) & int(y, 2))
-
 class Mask:
     def __init__(self, val):
-        self.__zero = "".join(('1' if char == 'X' else char for char in val))
-        self.__one = "".join(('0' if char == 'X' else char for char in val))
+        self.__x = "".join(('0' if char != 'X' else char for char in val))
+        self.__one = "".join(('0' if char != '1' else char for char in val))
 
     def apply(self, val):
-        return bin_or(self.__one, bin_and(val, self.__zero))
+        val = bin_or(self.__one, val)
+        val = "".join((x_val if x_val == 'X' else val for x_val, val in zip(self.__x, val)))
+        return self._get_all_possible_addrs(val)
+    
+    def _get_all_possible_addrs(self, val):
+        for i, char in enumerate(val):
+            if char == 'X':
+                val_zero = s = val[:i] + '0' + val[i + 1:]
+                val_one = s = val[:i] + '1' + val[i + 1:]
+                return self._get_all_possible_addrs(val_zero) + self._get_all_possible_addrs(val_one)   
+        return [val]
 
 class MemInstruction:
     def __init__(self, lval, rval):
@@ -21,8 +28,9 @@ class MemInstruction:
         self.__val = f'{int(rval):036b}'
 
     def exec(self, state):
-        state.mem[self.__addr] = state.mask.apply(self.__val)
-
+        for addr in state.mask.apply(self.__addr):
+            state.mem[addr] = self.__val    
+        
 class MaskInstruction:
     def __init__(self, rval):
         self.__mask = Mask(rval)
