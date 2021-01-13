55233a8932d990687086930eb17ac3a64bdc6210
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2021-01-13 11:36:50 +0000

    add solution for 22-2

--- 22/solution.py
+++ 22/solution.py
@@ -1,3 +1,5 @@
+import copy
+
 class Player:
     def __init__(self, name: str, deck: list):
         self.name = name
@@ -9,6 +11,9 @@ class Player:
     def play(self):
         return self.deck.pop(0)
     
+    def deck_size(self):
+        return len(self.deck)
+
     def has_lost(self):
         return len(self.deck) == 0
 
@@ -34,45 +39,75 @@ class Combat:
 
     def play(self) -> Player:
         while not self.is_finished():
-            plays = {player : player.play() for player in self.players}
-            winner = max(plays, key=plays.get)
-            played_cards = list(plays.values())
-            played_cards.sort(reverse=True)
-            winner.win(played_cards)
+            self.round()
             self.players = [player for player in self.players if not player.has_lost()]
         return self.players[0]
 
+    def round(self):
+        plays = {player : player.play() for player in self.players}
+        winner = max(plays, key=plays.get)
+        played_cards = list(plays.values())
+        played_cards.sort(reverse=True)
+        winner.win(played_cards)
+            
     def sum(self) -> int:
         return sum((player.sum() for player in self.players))
 
-class RecursiveCombatRound:
-    def __init__(self, players: set):
+class RecursiveCombat(Combat):
+    def __init__(self, players: set, num=1):
         self.players = players
         self.past_states = set()
-    
+        self.num = num
+        
     def is_finished(self) -> bool:
         return len(self.players) == 1
 
     def play(self) -> Player:
         while not self.is_finished():
-            if self.state() in self.past_states:
-                return self.players[0] # first player wins
-            self.past_states.add(self.state())
-            plays = {player : player.play() for player in self.players}
-            winner = max(plays, key=plays.get)
-            played_cards = list(plays.values())
-            played_cards.sort(reverse=True)
-            winner.win(played_cards)
+            curr_state = self.state()
+            if curr_state in self.past_states:
+                return self.players[0]
+            self.past_states.add(curr_state)
+
+            self.round()
             self.players = [player for player in self.players if not player.has_lost()]
+        return self.players[0]
+
+    def round(self):
+        plays = {player : player.play() for player in self.players}
+        winner = None
+        for player, card in plays.items():
+            if card > player.deck_size():
+                winner = max(plays, key=plays.get)
+                played_cards = list(plays.values())
+                played_cards.sort(reverse=True)
+                winner.win(played_cards)
+                return        
+        winner_name = self.play_subgame(plays)
+        for player in self.players:
+            if player.name == winner_name:
+                winner = player
+        played_cards = [plays[winner]]
+        for player in self.players:
+            if player.name != winner_name:
+                played_cards.append(plays[player])
+        winner.win(played_cards)
+        
+    def play_subgame(self, plays):
+        player_copies = list()
+        for player, cards in plays.items():
+            new_player = Player(player.name, player.deck[:cards])
+            player_copies.append(new_player)
+        subgame = RecursiveCombat(player_copies, self.num + 1)
+        return subgame.play().name
             
     def sum(self) -> int:
         return sum((player.sum() for player in self.players))
-    
-    def state(self):
-        return [(player.name, player.deck) for player in self.players]
 
+    def state(self) -> str:
+        return "".join([str(player) for player in self.players])
 
-
+        
 def parse_player(deck: str) -> Player:
     name, *cards = deck.split('\n')
     cards = [int(card) for card in cards]
@@ -86,8 +121,13 @@ def parse_input(path:str) -> set:
         return {parse_player(player) for player in players}
 players = parse_input('input.txt')
 
-game = Combat(players)
+game = Combat(copy.deepcopy(players))
 
 game.play()
+
+game = RecursiveCombat(players)
+
+game.play()
+
 
 print(game.sum())
