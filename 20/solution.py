6d290cd1947dc2b6279ccc61c0f52078722ac06a
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2021-01-14 16:21:36 +0000

    assemble tiles in day 20

--- 20/solution.py
+++ 20/solution.py
@@ -1,7 +1,11 @@
 import itertools
 import functools
+import random
 
 class Tile:
+    contrary_sides = {'top': 'bot', 'bot': 'top', 'left': 'right', 'right': 'left'}
+    flipped_sides = {'top': 'bot', 'bot': 'top', 'left': 'left', 'right': 'right'}
+    rotated_sides = {'top': 'right', 'right': 'bot', 'bot': 'left', 'left': 'top'}
     def __init__(self, name: str, image: list):
         self.number = int(name[-6:-1])
         self.image = image
@@ -16,6 +20,64 @@ class Tile:
         
     def connects_to(self, other) -> bool:
         return self.possible_bounds & other.possible_bounds
+    
+    def get_matching_side(self, other) -> str:
+        for side, bound in self.bounds.items():
+            if bound == other.bounds[Tile.contrary_sides[side]]:
+                return side
+        return None
+    
+    def connect(self, other):
+        if other in self.neighbours.values():
+            return
+
+        for i in range(4):
+            matching_side = self.get_matching_side(other)
+            if matching_side is not None:
+                self.neighbours[matching_side] = other
+                other.neighbours[Tile.contrary_sides[matching_side]] = self
+                return
+            self.rotate(set())
+
+        self.flip(set())
+
+        for i in range(4):
+            matching_side = self.get_matching_side(other)
+            if matching_side is not None:
+                self.neighbours[matching_side] = other
+                other.neighbours[Tile.contrary_sides[matching_side]] = self
+                return
+            self.rotate(set())
+
+    def rotate(self, visited):
+        if self in visited:
+            return
+        visited.add(self)
+
+        self.bounds = {Tile.rotated_sides[side]: bound for side, bound in self.bounds.items()}
+        self.bounds['top'] = self.bounds['top'][::-1]
+        self.bounds['bot'] = self.bounds['bot'][::-1]
+        
+        self.neighbours = {Tile.rotated_sides[side]: neighbour for side, neighbour in self.neighbours.items()}
+        for neighbour in self.neighbours.values():
+            if neighbour is not None:
+                neighbour.rotate(visited)
+        return
+    
+    def flip(self, visited):
+        if self in visited:
+            return
+        visited.add(self)
+
+        self.bounds = {Tile.flipped_sides[side]: bound for side, bound in self.bounds.items()}
+        self.bounds['left'] = self.bounds['left'][::-1]
+        self.bounds['right'] = self.bounds['right'][::-1]
+        
+        self.neighbours = {Tile.flipped_sides[side]: neighbour for side, neighbour in self.neighbours.items()}
+        for neighbour in self.neighbours.values():
+            if neighbour is not None:
+                neighbour.flip(visited)
+        return
     
     def __repr__(self):
         return str(self.number)
@@ -37,6 +99,11 @@ def get_connections(tiles: list) -> dict:
             connection_map[second].append(first)
     return connection_map
 
+def connect_tiles(connection_map: dict):
+    for tile, connections in connection_map.items():
+        for connection in connections:
+            tile.connect(connection)
+
 def get_borders(connection_map: dict) -> list:
     return [tile for tile, connections in connection_map.items() if len(connections) == 2]
 
@@ -47,3 +114,10 @@ connection_map = get_connections(tiles)
 border_tiles = get_borders(connection_map)
 
 print(functools.reduce(lambda x, y: x * y, map(lambda x: x.number, border_tiles)))
+
+connect_tiles(connection_map)
+
+tiles[0].flip(set())
+
+for tile in tiles:
+    print(f'tile:{tile.number}, neighbours: {tile.neighbours}')
